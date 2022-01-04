afe0c00baf77a8f9115cd5f643af1e0b148785f0
991154416@qq.com <root@localhost.localdomain>
2022-01-04 11:49:35 +0800

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-<<<<<<< HEAD
 import { CreateRepositoryCommand, ECRClient } from "@aws-sdk/client-ecr";
 
 async function run(): Promise<void> {
@@ -22,22 +21,6 @@ async function run(): Promise<void> {
     // error handling.
   } finally {
     // finally.
-=======
-import {wait} from './wait'
-
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
->>>>>>> parent of fdbb3a9 (ecr create repository)
   }
 }
 
